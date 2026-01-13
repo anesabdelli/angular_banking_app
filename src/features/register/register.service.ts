5b368e254ff10152adb71476622f3a712246d05c
anesabdelli <anesabdelli6662@gmail.com>
2026-01-13 15:38:44 +0100

    fix branch register using initial code without SSR, add basic adapter pattern code

---
+++ src/features/register/register.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface RegisterDTO {
+  name: string;
+  password: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterApiService {
+  private apiUrl = 'https://coding-bank.fly.dev/auth/register';
+
+  constructor(private http: HttpClient) {}
+
+  registerApi(dto: RegisterDTO): Observable<RegisterDTO> {
+    console.log('RegisterApiService: Sending registration data to API', dto);
+    return this.http.post<RegisterDTO>(this.apiUrl, dto);
+  }
+}
