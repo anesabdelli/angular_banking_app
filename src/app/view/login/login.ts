57ab58481a79d1f40148371f44d3af62ce9b76d9
CHARLESEmm <ccharlesemmanuel@gmail.com>
2026-01-16 14:33:35 +0100

    fix timeout

--- src/app/view/login/login.ts
+++ src/app/view/login/login.ts
@@ -103,9 +103,9 @@ clearInput() {
         localStorage.setItem('user', JSON.stringify(result.user));
 
         this.loginSuccess.set(true);
-        setTimeout(() => {
+       
           this.router.navigate(['/account']);
-        }, 1000);
+
       } catch (error) {
         this.loginError.set('Code client ou mot de passe incorrect');
         console.error('Login failed', error);
